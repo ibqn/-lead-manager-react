6177382c6a332e387190d7b2319982b5eb2656c8
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-06-01 20:23:52 +0200

    obtain list of leads from rest api

--- src/components/leads/leads.jsx
+++ src/components/leads/leads.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getLeads } from '../../reducers/leads/actions'
 
 const Leads = () => {
+  const leads = useSelector((state) => state.leads)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    console.log('get leads')
+    dispatch(getLeads())
+  }, [dispatch])
+
   return (
     <>
       <h1>Leads</h1>
+      {JSON.stringify(leads)}
     </>
   )
 }
