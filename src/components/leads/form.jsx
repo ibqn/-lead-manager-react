8163f5e66003df33649ed1643591b4c7783f267b
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-06-01 18:38:36 +0200

    improve form components

--- src/components/leads/form.jsx
+++ src/components/leads/form.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initLead = {
+  name: '',
+  email: '',
+  message: '',
+}
 
 const Form = () => {
+  const [lead, setLead] = useState(initLead)
+
+  const handleChange = (name) => ({ target: { value } }) =>
+    setLead({ ...lead, [name]: value })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(lead)
+    setLead(initLead)
+  }
+
   return (
-    <>
-      <h1>Add Leads</h1>
-    </>
+    <div className="card card-body mt-4 mb-4">
+      <h1>Add Lead</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            onChange={handleChange('name')}
+            value={lead.name}
+          />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            className="form-control"
+            type="email"
+            name="email"
+            onChange={handleChange('email')}
+            value={lead.email}
+          />
+        </div>
+        <div className="form-group">
+          <label>Message</label>
+          <textarea
+            className="form-control"
+            name="message"
+            onChange={handleChange('message')}
+            value={lead.message}
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
