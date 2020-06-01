643b856372ea660c13229e661ba03a709fb423aa
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-06-01 11:41:28 +0200

    add layout and dashboard components

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
-import logo from './logo.svg'
 import './app.css'
 
-function App() {
+import { Header } from './components/layout'
+import { Dashboard } from './components/leads'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <Dashboard />
+      </div>
+    </>
   )
 }
 
