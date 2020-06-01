b7b12a5a66be14df9994281c34e4a1769224335d
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-06-01 21:07:32 +0200

    complete leads list

--- src/components/leads/leads.jsx
+++ src/components/leads/leads.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getLeads } from '../../reducers/leads/actions'
+import { getLeads, deleteLead } from '../../reducers/leads/actions'
 
 const Leads = () => {
   const leads = useSelector((state) => state.leads)
@@ -14,8 +14,36 @@ const Leads = () => {
 
   return (
     <>
-      <h1>Leads</h1>
-      {JSON.stringify(leads)}
+      <h1>Leads List</h1>
+      <table className="table table-stripped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Message</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {leads.leads.map((lead) => (
+            <tr key={lead.id}>
+              <td>{lead.id}</td>
+              <td>{lead.name}</td>
+              <td>{lead.email}</td>
+              <td>{lead.message}</td>
+              <td>
+                <button
+                  onClick={() => dispatch(deleteLead(lead.id))}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
