2943abb51c5a3f0e4998a967bb62eb84b87c758a
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-06-01 12:48:01 +0200

    add bootswatch with cosmo theme. add basic header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
 
-const Header = () => <div>Header</div>
+const Header = () => (
+  <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Navbar.Brand href="#">Lead Managers</Navbar.Brand>
+  </Navbar>
+)
 
 export default Header
