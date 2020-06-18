48024fa200e222e77498ab290b6015edba7a962e
ibqn <evgen.ibqn@gmail.com>
2020-06-18 23:38:49 +0200

    add basic router

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,21 @@ import React from 'react'
 import { Header, Alerts } from './components/layout'
 import { Dashboard } from './components/leads'
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
         <Alerts />
-        <Dashboard />
+        <Switch>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
       </div>
-    </>
+    </Router>
   )
 }
 
