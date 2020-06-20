88b4493c4bd8c00188a29272898f4ee15aaed149
ibqn <evgen.ibqn@gmail.com>
2020-06-20 22:20:17 +0200

    use router link in the header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 
 const Header = () => (
   <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-    <Navbar.Brand href="#">Lead Managers</Navbar.Brand>
+    <Navbar.Brand as={Link} to="/" href="/">
+      Lead Managers
+    </Navbar.Brand>
   </Navbar>
 )
 
