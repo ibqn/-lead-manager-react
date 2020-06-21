e2d55d34fafdec4d7c69c17bf324505e62d176b8
ibqn <evgen.ibqn@gmail.com>
2020-06-22 01:03:15 +0200

    add navigation links to header

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 const Header = () => (
   <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
     <Navbar.Brand as={Link} to="/" href="/">
       Lead Managers
     </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link as={Link} to="/login" href="/login">
+          sign in
+        </Nav.Link>
+        <Nav.Link as={Link} to="/register" href="/register">
+          sign up
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
 )
 
