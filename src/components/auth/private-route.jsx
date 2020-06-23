d662a39e882a0a7c54b939cc71ba120de9430bb6
ibqn <evgen.ibqn@gmail.com>
2020-06-23 16:19:22 +0200

    implement private route

--- src/components/auth/private-route.jsx
+++ src/components/auth/private-route.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const PrivateRoute = ({ children, ...rest }) => {
+  const auth = useSelector((state) => state.auth)
+
   return (
     <Route
       {...rest}
       render={({ location }) => {
-        return children
+        if (auth.isLoading) {
+          return <h2 className="text-center mt-5">Loading...</h2>
+        } else if (auth.isAuthenticated) {
+          return children
+        }
+
+        return (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+        )
       }}
     />
   )
