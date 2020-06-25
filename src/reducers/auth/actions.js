4a6b8286ae1c1f8bbf9110dc7181b6e86a4f9645
ibqn <evgen.ibqn@gmail.com>
2020-06-25 23:12:08 +0200

    remove commented out code

--- src/reducers/auth/actions.js
+++ src/reducers/auth/actions.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import { useStore } from 'react-redux'
 
 import {
   USER_LOADING,
@@ -18,27 +17,14 @@ const loadUser = (token) => async (dispatch) => {
     headers: { 'content-Type': 'application/json' },
   }
 
-  console.log(token)
-
   if (token) {
     config.headers['Authorization'] = `Token ${token}`
   }
 
   try {
     const response = await axios.get('/api/auth/user', config)
-    console.log('response', response.data)
     dispatch({ type: USER_LOADED, payload: response.data })
   } catch (error) {
-    console.log('error response', error.response)
-    // const { data: message, status } = error.response
-    // console.log('message', message, 'status', status)
-    // dispatch(
-    //   setError({
-    //     message: JSON.stringify(message),
-    //     status,
-    //     timestamp: Date.now(),
-    //   })
-    // )
     dispatch({ type: LOGIN_FAILURE })
   }
 }
@@ -69,7 +55,6 @@ const logoutUser = (token) => async (dispatch) => {
     headers: { 'content-Type': 'application/json' },
   }
 
-  console.log('logout action token', token)
   if (token) {
     config.headers['Authorization'] = `Token ${token}`
   }
